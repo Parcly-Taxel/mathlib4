17a1aeef798a6bbbdb540b7eb9c5863e5f404c0a
Parcly Taxel <reddeloostw@gmail.com>
2026-01-29 13:53:54 +0800

    oops

--- Archive/Imo/Imo2002Q3.lean
+++ Archive/Imo/Imo2002Q3.lean
@@ -3,7 +3,8 @@ Copyright (c) 2026 Jeremy Tan. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jeremy Tan
 -/
-import Mathlib
+import Mathlib.Analysis.Polynomial.Basic
+import Mathlib.RingTheory.Polynomial.UniqueFactorization
 
 /-!
 # IMO 2002 Q3
